cb189437c2821c4049da05befff9058d6aa787da
ValonS95 <valonsadiku123@gmail.com>
2025-11-30 21:04:12 +0100

    Commit 3: Updated version of test_validator

--- test_validator.py
+++ test_validator.py
@@ -1,5 +1,17 @@
 from user_input_validator import UserInputValidator
 
-# Create two instances of UserInputValidator
+# Create two instances
 validator1 = UserInputValidator()
 validator2 = UserInputValidator()
+
+# Sample input lists
+inputs1 = ["10", "abc", "-5", "3"]
+inputs2 = ["7", "0", "12", "xyz"]
+
+# Validate inputs
+valid1 = validator1.validate_positive_integers(inputs1)
+valid2 = validator2.validate_positive_integers(inputs2)
+
+# Display validation results
+validator1.display_validation_message()
+validator2.display_validation_message()
